ec30b7b22a26ce3d3b48e8534fcabd4c8149f108
Sarper447 <68636515+Sarper447@users.noreply.github.com>
2022-08-02 16:25:19 +0200

    Update link.py

--- link.py
+++ link.py
@@ -54,3 +54,100 @@ while(True):
 cap.release()
 
 
+
+
+caliration
+
+# Different directories for each camera
+LEFT_PATH = "capture/left/{:06d}.jpg"
+RIGHT_PATH = "capture/right/{:06d}.jpg"
+
+# Filenames are just an increasing number
+frameId = 0
+
+# Capture loop from earlier...
+while(True):
+    # Actually save the frames
+    cv2.imwrite(LEFT_PATH.format(frameId), leftFrame)
+    cv2.imwrite(RIGHT_PATH.format(frameId), rightFrame)
+    frameId += 1
+    
+    
+    MAX_IMAGES = 64
+if (len(filenames) > MAX_IMAGES):
+    print("Too many images to calibrate, using {0} randomly selected images"
+            .format(MAX_IMAGES))
+    filenames = random.sample(filenames, MAX_IMAGES)
+    
+    
+    
+    CROP_WIDTH = 960
+def cropHorizontal(image):
+    return image[:,
+            int((CAMERA_WIDTH-CROP_WIDTH)/2):
+            int(CROP_WIDTH+(CAMERA_WIDTH-CROP_WIDTH)/2)]
+
+
+(_, _, _, _, _, rotationMatrix, translationVector, _, _) = cv2.stereoCalibrate(
+        objectPoints, leftImagePoints, rightImagePoints,
+        leftCameraMatrix, leftDistortionCoefficients,
+        rightCameraMatrix, rightDistortionCoefficients,
+        imageSize, None, None, None, None,
+        cv2.CALIB_FIX_INTRINSIC, TERMINATION_CRITERIA)
+
+
+(leftRectification, rightRectification, leftProjection, rightProjection,
+        dispartityToDepthMap, leftROI, rightROI) = cv2.stereoRectify(
+                leftCameraMatrix, leftDistortionCoefficients,
+                rightCameraMatrix, rightDistortionCoefficients,
+                imageSize, rotationMatrix, translationVector,
+                None, None, None, None, None,
+                cv2.CALIB_ZERO_DISPARITY, OPTIMIZE_ALPHA)
+
+leftMapX, leftMapY = cv2.initUndistortRectifyMap(
+        leftCameraMatrix, leftDistortionCoefficients, leftRectification,
+        leftProjection, imageSize, cv2.CV_32FC1)
+rightMapX, rightMapY = cv2.initUndistortRectifyMap(
+        rightCameraMatrix, rightDistortionCoefficients, rightRectification,
+        rightProjection, imageSize, cv2.CV_32FC1)
+
+
+np.savez_compressed(outputFile, imageSize=imageSize,
+        leftMapX=leftMapX, leftMapY=leftMapY, leftROI=leftROI,
+        rightMapX=rightMapX, rightMapY=rightMapY, rightROI=rightROI)
+
+depth map
+
+calibration = np.load(sys.argv[1], allow_pickle=False)
+imageSize = tuple(calibration["imageSize"])
+leftMapX = calibration["leftMapX"]
+leftMapY = calibration["leftMapY"]
+leftROI = tuple(calibration["leftROI"])
+rightMapX = calibration["rightMapX"]
+rightMapY = calibration["rightMapY"]
+rightROI = tuple(calibration["rightROI"])
+
+
+stereoMatcher = cv2.StereoBM_create()
+
+fixedLeft = cv2.remap(leftFrame, leftMapX, leftMapY, REMAP_INTERPOLATION)
+fixedRight = cv2.remap(rightFrame, rightMapX, rightMapY, REMAP_INTERPOLATION)
+
+grayLeft = cv2.cvtColor(fixedLeft, cv2.COLOR_BGR2GRAY)
+grayRight = cv2.cvtColor(fixedRight, cv2.COLOR_BGR2GRAY)
+depth = stereoMatcher.compute(grayLeft, grayRight)
+
+stereoMatcher = cv2.StereoBM_create()
+
+fixedLeft = cv2.remap(leftFrame, leftMapX, leftMapY, REMAP_INTERPOLATION)
+fixedRight = cv2.remap(rightFrame, rightMapX, rightMapY, REMAP_INTERPOLATION)
+
+grayLeft = cv2.cvtColor(fixedLeft, cv2.COLOR_BGR2GRAY)
+grayRight = cv2.cvtColor(fixedRight, cv2.COLOR_BGR2GRAY)
+depth = stereoMatcher.compute(grayLeft, grayRight)
+
+stereoMatcher.setMinDisparity(4)
+stereoMatcher.setNumDisparities(128)
+stereoMatcher.setBlockSize(21)
+stereoMatcher.setSpeckleRange(16)
+stereoMatcher.setSpeckleWindowSize(45)
